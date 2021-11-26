48504a8aceca73ece7e435bc6650be85a42e67c0
Jerry Tong <zihangtong@gmail.com>
2021-11-26 00:34:10 -0500

    commit tree ce71111291beac18faf0411d55ab01572f092d80

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const headQuarters = 42;
+
+function distanceFromHqInBlocks(pickUp) {
+    return Math.abs(pickUp - headQuarters);
+}
+
+function distanceFromHqInFeet(pickUp) {
+    return distanceFromHqInBlocks(pickUp) * 264;
+}
+
+function distanceTravelledInFeet(pickUp, headQuarters) {
+    return Math.abs(pickUp - headQuarters) * 264;
+}
+
+function calculatesFarePrice(pickUp, headQuarters) {
+    const distance = distanceTravelledInFeet(pickUp, headQuarters);
+    if (distance <= 400) {
+        return 0;
+    }  else if (distance <= 2000) {
+        return (distance - 400) * .02;
+    } else if (distance < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
